8b943242d8798aa7ec560207efeeaa6ed42c5066
alexjalk <alexjalk@umich.edu>
2019-02-14 21:08:38 -0500

    Add google login
    Added login and connection to google developer site.
    Also implemented Firebase
    Note: Only works for android currently.

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, Text, Button, Image } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
+  //Get info from google sign in
+  constructor(props) {
+    super(props)
+    this.state = {signedIn: false, name: "", photoUrl: ""}
+  } 
+  //Sign in with google
+  signIn = async () => {
+    try {
+      const result = await Expo.Google.logInAsync({
+        androidClientId:
+          "714781313345-6sg5819b8bk8kta4fjgm86vm6fdq6rs6.apps.googleusercontent.com",
+        //iosClientId: YOUR_CLIENT_ID_HERE,  <-- Todo: add IOS client ID
+        scopes: ["profile", "email"]
+      })
+
+      if (result.type === "success") {
+        this.setState({
+          signedIn: true,
+          name: result.user.name,
+          photoUrl: result.user.photoUrl
+        })
+      } else {
+        console.log("cancelled")
+      }
+    } catch (e) {
+      console.log("error", e)
+    }
+  }
   state = {
     isLoadingComplete: false,
   };
@@ -20,6 +48,11 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
+          {this.state.signedIn ? (
+            <LoggedInPage name={this.state.name} photoUrl={this.state.photoUrl} />
+          ) : (
+            <LoginPage signIn={this.signIn} />
+          )}
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
           <AppNavigator />
         </View>
@@ -54,9 +87,38 @@ export default class App extends React.Component {
   };
 }
 
+const LoginPage = props => {
+  return (
+    <View>
+      <Text style={styles.header}>Sign In With Google</Text>
+      <Button title="Sign in with Google" onPress={() => props.signIn()} />
+    </View>
+  )
+}
+
+const LoggedInPage = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Welcome:{props.name}</Text>
+      <Image style={styles.image} source={{ uri: props.photoUrl }} />
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 25
+  },
+  image: {
+    marginTop: 15,
+    width: 150,
+    height: 150,
+    borderColor: "rgba(0,0,0,0.2)",
+    borderWidth: 3,
+    borderRadius: 150
   },
 });
