00b757cb754311af74472b92b179f3111490e018
ericlian <ericlian@umich.edu>
2019-02-21 20:03:31 -0500

    Search bar

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,16 +1,103 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
+import React, { PureComponent } from 'react';
+import { TouchableHightLight, Text, View } from 'react-native';
+import AtoZListView from 'react-native-atoz-listview';
+import Search from 'react-native-search-box';
 
-export default class SearchScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Search',
-  };
+const rowHeight = 40;
+
+class SearchScreen extends PureComponent {
+
+  state = {
+    data: {
+      "A": [
+        {
+          "name": "Anh Tuan Nguyen",
+          "age": 28
+        },
+        {
+          "name": "An Nhien",
+          "age": 2
+        },
+      ],
+      "Z": [
+        {
+          "name": "Thanh Tu Pham",
+          "age": 32
+        },
+        {
+          "name": "Tien Thanh",
+          "age": 24
+        },
+      ]
+    }
+  }
+
+    renderRow = (item, sectionId, index) => {
+      return (
+        <TouchableHightLight
+          style={{
+            height: rowHeight,
+            justifyContent: 'center',
+            alignItems: 'center'}}
+        >
+          <Text>{item.name}</Text>
+        </TouchableHightLight>
+      );
+    }
+
+    // Important: You must return a Promise
+    onSearch = (searchText) => {
+        return new Promise((resolve, reject) => {
+            console.log(searchText);
+            console.log('Add your search function here.');
+            resolve();
+        });
+    }
+    
+    // Important: You must return a Promise
+    beforeFocus = () => {
+        return new Promise((resolve, reject) => {
+            console.log('beforeFocus');
+            resolve();
+        });
+    }
+
+    // Important: You must return a Promise
+    onFocus = (text) => {
+        return new Promise((resolve, reject) => {
+            console.log('onFocus', text);
+            resolve();
+        });
+    }
+
+    // Important: You must return a Promise
+    afterFocus = () => {
+        return new Promise((resolve, reject) => {
+            console.log('afterFocus');
+            resolve();
+        });
+    }
 
   render() {
-  	return(
-  		<View>
-  			<Text>Search Screen</Text>
-  		</View>
-  	);
+    // inside your render function
+    return (
+      <View style={{ flex: 1}}>
+        <Search
+          ref="search_box"
+          onSearch={this.onSearch}
+          /**
+          * There many props that can customizable
+          * Please scroll down to Props section
+          */
+        />
+
+        <AtoZListView
+          data={this.state.data}
+          renderRow={this.renderRow}
+          rowHeight={rowHeight}
+          sectionHeaderHeight={40}
+        />
+      </View>
+    );
   }
 }
